bc784619dc243c902779fea0938fd4b1367a3e61
desertsniper87 <torshobuet@gmail.com>
2019-04-20 23:02:35 +0600

    testing force push

--- scripts/publish_to_github_pages.sh
+++ scripts/publish_to_github_pages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pelican content -o output -s pelicanconf.py
-ghp-import output  
-git push git@github.com:desertsniper87/desertsniper87.github.io.git gh-pages:master
+ghp-import output
+git push git@github.com:desertsniper87/desertsniper87.github.io.git gh-pages:master --force
 
