e8ea92ae50f42252cde2835559b971543db7a0e9
desertsniper87 <torshobuet@gmail.com>
2022-10-21 21:23:34 +0600

    bad revision

---
+++ script-for-standalone-gh.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env #!/bin/bash
+
+
+git clone git@github.com:desertSniper87/desertSniper87.github.io.github
+mv desertSniper87.github.io .output-git
+
+rm .output-git/* -r
+cp output/* .output-git/ -r
+
+cd .output-git
+git commit -am 'master branch update'
+git push origin master
+
